97ae13fd90b824d6c6d4f82145d5070bd98ab0af
babiya88 <dheeshanadewmini12@gmail.com>
2024-08-23 19:25:47 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,4 +6,6 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
+ALIVE_IMG: process.env.ALIVE_IMG || "ALIVE PHOTO එකට ඕනි URL එක මෙතන්ට දෙන්න",
+ALIVE_MSG: process.env.ALIVE_MSG ||  "Hello, I am King Babiya whatsapp user bot i am alive now",
 };
